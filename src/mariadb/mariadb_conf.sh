9f6f39d9cd74b692f343156ef93453994f199d26
ouhassna <ouhassna@ouhassna.ouhassna>
2024-03-22 16:36:53 -0400

    running nginx , mariadb, wordpres , and nginx serve wordpress

---
+++ src/mariadb/mariadb_conf.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ ! -d "/run/mysqld" ]; then
+	echo "[ mariadb ] creating mysqld folder..."
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+echo "[ mariadb ] enabling network access to mariadb server..."
+sed -i "s|skip-networking|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|#bind-address=0.0.0.0|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	chown -R mysql:mysql /var/lib/mysql
+	
+	echo "[ mariadb ] mariadb installation..."
+	mariadb-install-db --datadir=/var/lib/mysql --user=mysql --skip-test-db
+
+	echo "[ mariadb ] configuring mariadb..."
+	file=/tmp/.tmp
+	cat << EOF > $file
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
+CREATE DATABASE $WP_DB_NAME;
+CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+	mariadbd --user=mysql --bootstrap < $file
+	rm -f $file
+fi
+
+echo "[ mariadb ] starting mariadb in the foreground..."
+exec /usr/bin/mysqld --user=mysql --console &>/dev/null
+
---
+++ src/nginx/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[ generating ssl certificates ]
+echo "[ nginx ] generating ssl certificate..."
+mkdir -p /etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/nginx/ssl/ouhassna.key \
+    -out /etc/nginx/ssl/ouhassna.crt  \
+    -subj "/C=MA/L=Martil/O=42/OU=1337 MED/CN=ouhassna.42.fr" &>/dev/null
+echo "[ nginx ] a private key and certificate generated successfully."
+# [ starting nginx process ]
+echo "[ nginx ] starting nginx in the foreground..."
+nginx -g "daemon off;"
